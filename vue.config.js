f937dc8f1ce6278fcf9a2acfd6cdde8bc6fc6f50
maximnacd <129396595+maximnacd@users.noreply.github.com>
2023-03-30 17:12:52 +0300

    Deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-app/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/mysite/" : "/",
 });
