cd9a986576b3d56f3cdd3d5a9ff71cd82c29da43
Maxim Zugan <maxspbfox@gmail.com>
2023-03-30 18:39:05 +0300

    Deployment

---
+++ js/about.24f034c4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_app"]=self["webpackChunkvue_app"]||[]).push([[443],{7381:function(n,u,a){a.r(u),a.d(u,{default:function(){return f}});var e=a(3396);const t={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,e.wg)(),(0,e.iD)("div",t,s)}var i=a(89);const p={},o=(0,i.Z)(p,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=about.24f034c4.js.map
